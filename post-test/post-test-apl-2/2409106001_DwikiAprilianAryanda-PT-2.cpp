8f02292a81779494d3232b6b4c4b4d33f3404a00
DwikiAprilianAryanda <aprilianaryandad@gmail.com>
2025-03-15 13:05:01 +0700

    Finish Post Test 2
    perbaiki laporan sama program dikit

--- post-test/post-test-apl-2/2409106001_DwikiAprilianAryanda-PT-2.cpp
+++ post-test/post-test-apl-2/2409106001_DwikiAprilianAryanda-PT-2.cpp
@@ -10,8 +10,7 @@ string merkProduk[100];
 int hargaProduk[100];
 int jumlahData = 0;
 
-// Fungsi login
-int login() {
+int main() {
     string nama, nim;
     string namabenar = "lian";
     string nimbenar = "001";
@@ -28,112 +27,18 @@ int login() {
         
         if (nama == namabenar && nim == nimbenar) {
             cout << "Login berhasil!" << endl;
-            return true;
+            break;
         } else {
             percobaan++;
             cout << "Login gagal! Sisa percobaan: " << (3 - percobaan) << endl;
         }
     }
-    cout << "Cukurukuk gak bisa nyoba login lagi" << endl;
-    return false;
-}
-
-// Fungsi tampilData
-int tampilData() {
-    if (jumlahData == 0) {
-        cout << "Tidak ada data handphone!" << endl;
+    if (percobaan >= 3) {
+        cout << "Cukurukuk gak bisa nyoba login lagi" << endl;
         return 0;
     }
-    
-    cout << "\n=== Data Handphone ===" << endl;
-    cout << setw(10) << "Kode" << setw(20) << "Nama Produk" << setw(15) << "Merk" << setw(15) << "Harga" << endl;
-    cout << string(60, '-') << endl;
-    
-    for (int i = 0; i < jumlahData; i++) {
-        cout << setw(10) << kodeProduk[i] 
-             << setw(20) << namaProduk[i]
-             << setw(15) << merkProduk[i] 
-             << setw(15) << hargaProduk[i] << endl;
-    }
-    return jumlahData;
-}
 
-// Fungsi tambahData
-bool tambahData() {
-    if (jumlahData >= 100) {
-        cout << "Kapasitas penuh!" << endl;
-        return false;
-    }
-    
-    cout << "\n=== Tambah Data Handphone ===" << endl;
-    cout << "Masukkan Kode Produk: ";
-    cin >> kodeProduk[jumlahData];
-    cout << "Masukkan Nama Produk: ";
-    cin.ignore();
-    getline(cin, namaProduk[jumlahData]);
-    cout << "Masukkan Merk Produk: ";
-    getline(cin, merkProduk[jumlahData]);
-    cout << "Masukkan Harga: ";
-    cin >> hargaProduk[jumlahData];
-    
-    jumlahData++;
-    cout << "Data berhasil ditambahkan!" << endl;
-    return true;
-}
-
-// Fungsi ubahData
-bool ubahData() {
-    string kode;
-    cout << "\nMasukkan Kode Produk yang akan diubah: ";
-    cin >> kode;
-    
-    for (int i = 0; i < jumlahData; i++) {
-        if (kodeProduk[i] == kode) {
-            cout << "Masukkan Nama Produk baru: ";
-            cin.ignore();
-            getline(cin, namaProduk[i]);
-            cout << "Masukkan Merk Produk: ";
-            cin.ignore();
-            getline(cin, merkProduk[i]);
-            cout << "Masukkan Harga baru: ";
-            cin >> hargaProduk[i];
-            cout << "Data berhasil diubah!" << endl;
-            return true;
-        }
-    }
-    cout << "Kode produk tidak ditemukan!" << endl;
-    return false;
-}
-
-// Fungsi hapusData
-bool hapusData() {
-    string kode;
-    cout << "\nMasukkan Kode Produk yang akan dihapus: ";
-    cin >> kode;
-    
-    for (int i = 0; i < jumlahData; i++) {
-        if (kodeProduk[i] == kode) {
-            for (int j = i; j < jumlahData - 1; j++) {
-                kodeProduk[j] = kodeProduk[j + 1];
-                namaProduk[j] = namaProduk[j + 1];
-                merkProduk[j] = merkProduk[j + 1];
-                hargaProduk[j] = hargaProduk[j + 1];
-            }
-            jumlahData--;
-            cout << "Data berhasil dihapus!" << endl;
-            return true;
-        }
-    }
-    cout << "Kode produk tidak ditemukan!" << endl;
-    return false;
-}
-
-// Menu utama
-int main() {
-    if (!login()) {
-        return 0;
-    }
-    
+    // Menu utama
     char ulang;
     do {
         cout << "==================================\n";
@@ -151,21 +56,97 @@ int main() {
         cin >> pilihan;
         
         switch (pilihan) {
-            case 1:
-                tampilData();
+            case 1: // Tampilkan Data
+                if (jumlahData == 0) {
+                    cout << "Tidak ada data handphone!" << endl;
+                } else {
+                    cout << "\n=== Data Handphone ===" << endl;
+                    cout << setw(10) << "Kode" << setw(20) << "Nama Produk" << setw(15) << "Merk" << setw(15) << "Harga" << endl;
+                    cout << string(60, '-') << endl;
+                    for (int i = 0; i < jumlahData; i++) {
+                        cout << setw(10) << kodeProduk[i] 
+                             << setw(20) << namaProduk[i]
+                             << setw(15) << merkProduk[i] 
+                             << setw(15) << hargaProduk[i] << endl;
+                    }
+                }
                 break;
-            case 2:
-                tambahData();
+
+            case 2: // Tambah Data
+                if (jumlahData >= 100) {
+                    cout << "Kapasitas penuh!" << endl;
+                } else {
+                    cout << "\n=== Tambah Data Handphone ===" << endl;
+                    cout << "Masukkan Kode Produk: ";
+                    cin >> kodeProduk[jumlahData];
+                    cout << "Masukkan Nama Produk: ";
+                    cin.ignore();
+                    getline(cin, namaProduk[jumlahData]);
+                    cout << "Masukkan Merk Produk: ";
+                    getline(cin, merkProduk[jumlahData]);
+                    cout << "Masukkan Harga: ";
+                    cin >> hargaProduk[jumlahData];
+                    jumlahData++;
+                    cout << "Data berhasil ditambahkan!" << endl;
+                }
                 break;
-            case 3:
-                ubahData();
+
+            case 3: // Ubah Data
+                {
+                    string kode;
+                    cout << "\nMasukkan Kode Produk yang akan diubah: ";
+                    cin >> kode;
+                    bool ditemukan = false;
+                    for (int i = 0; i < jumlahData; i++) {
+                        if (kodeProduk[i] == kode) {
+                            cout << "Masukkan Nama Produk baru: ";
+                            cin.ignore();
+                            getline(cin, namaProduk[i]);
+                            cout << "Masukkan Merk Produk: ";
+                            getline(cin, merkProduk[i]);
+                            cout << "Masukkan Harga baru: ";
+                            cin >> hargaProduk[i];
+                            cout << "Data berhasil diubah!" << endl;
+                            ditemukan = true;
+                            break;
+                        }
+                    }
+                    if (!ditemukan) {
+                        cout << "Kode produk tidak ditemukan!" << endl;
+                    }
+                }
                 break;
-            case 4:
-                hapusData();
+
+            case 4: // Hapus Data
+                {
+                    string kode;
+                    cout << "\nMasukkan Kode Produk yang akan dihapus: ";
+                    cin >> kode;
+                    bool ditemukan = false;
+                    for (int i = 0; i < jumlahData; i++) {
+                        if (kodeProduk[i] == kode) {
+                            for (int j = i; j < jumlahData - 1; j++) {
+                                kodeProduk[j] = kodeProduk[j + 1];
+                                namaProduk[j] = namaProduk[j + 1];
+                                merkProduk[j] = merkProduk[j + 1];
+                                hargaProduk[j] = hargaProduk[j + 1];
+                            }
+                            jumlahData--;
+                            cout << "Data berhasil dihapus!" << endl;
+                            ditemukan = true;
+                            break;
+                        }
+                    }
+                    if (!ditemukan) {
+                        cout << "Kode produk tidak ditemukan!" << endl;
+                    }
+                }
                 break;
-            case 5:
+
+            case 5: // Keluar
                 cout << "Terima kasih!" << endl;
                 return 0;
+
             default:
                 cout << "Pilihan tidak valid!" << endl;
         }
